1916cdeb8d2828b9083bd61913930256c906b399
Havard-Tonning <havton24@gmail.com>
2025-04-27 16:17:16 +0300

    `Updated multiple HTML and JavaScript files with new content and Leaflet map configurations.`

--- huaren.js
+++ huaren.js
@@ -1,4 +1,4 @@
-var map = L.map('huaren_map', {zoomControl: false}).setView([61.845, 6.81], 14);
+var map = L.map('huaren_map', {zoomControl: false}).setView([61.845, 6.81], 13,8);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
