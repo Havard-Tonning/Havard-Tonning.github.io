f30b78f66d7dc164ee81c53fa1b2a08debd06a35
Havard-Tonning <havton24@gmail.com>
2025-06-11 14:32:33 +0200

    ´Added faq answers, info for hikes, daytrips with car, and a footer´

---
+++ toilet.js
@@ -0,0 +1,19 @@
+var map = L.map('toilet_map', {zoomControl: false}).setView([61.837268553540305, 6.807555672099274], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([61.83545797623981, 6.805650130396526]).addTo(map)
+    .bindPopup("<H3>Toilet</H3><img src='./images/toilet1.jpg' class='toiletimg'>");
+
+L.marker([61.836826234006985, 6.806368962568347]).addTo(map)
+    .bindPopup("<H3>Toilet</H3><img src='./images/toilet2.jpg' class='toiletimg'>");
+
+L.marker([61.838870667881864, 6.809051973013834]).addTo(map)
+    .bindPopup("<H3>Toilet</H3><img src='./images/toilet3.jpg' class='toiletimg'>");
+
+L.marker([61.85984345786699, 6.814774301942007]).addTo(map)
+    .bindPopup("<H3>Toilet</H3><img src='./images/toilet4.jpg' class='toiletimg'>");
+
+
--- utilities.js
+++
@@ -1,5 +0,0 @@
-var map = L.map('utilities_map', {zoomControl: false}).setView([61.845, 6.81], 13,8);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
