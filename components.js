63f789a042158d26bb2a604c5a4f0aff51ded9b6
Havard-Tonning <havton24@gmail.com>
2026-01-26 18:44:38 +0200

    Moved hamburger and gtag to components.

--- components.js
+++ components.js
@@ -12,4 +12,51 @@ class SiteFooter extends HTMLElement {
     }
 }
 
+class gtag extends HTMLElement {
+    connectedCallback() {
+        const gtagScript = document.createElement('script');
+        gtagScript.async = true;
+        gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-0LT1EF01DF';
+        document.head.appendChild(gtagScript);
+
+        const configScript = document.createElement('script');
+        configScript.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0LT1EF01DF');
+        `;
+        document.head.appendChild(configScript);
+    }
+}
+
+class hamburger extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `    <div class="menu-content">
+        <a href="index.html" style="margin-top: 100px;">Heim</a>
+        <a href="mainmap.html">Virtuell guida tur</a>
+        <a href="picktrip.html">Trimturar i området</a>
+        <a href="sites.html">Ein kort køyretur unna</a>
+        <a href="weather.html">Vêrmelding</a>
+        <a href="bus.html">Buss til Briksdalen</a>
+        <a href="toilet.html">Offentlege toalett</a>
+        <a href="faq.html">Vanlege spørsmål</a>
+        <a href="comments.html">Digital gjestebok</a>
+        <a href="feedback.html">Gi tilbakemelding til bygda</a>
+    </div>`; 
+
+        const hamMenu = document.querySelector('.ham-menu');
+        const menuContent = this.querySelector('.menu-content');
+
+        if (hamMenu && menuContent) {
+            hamMenu.addEventListener('click', () => {
+                hamMenu.classList.toggle('active');
+                menuContent.classList.toggle('active');
+            });
+        }
+    }
+}
+
 customElements.define('site-footer', SiteFooter);
+customElements.define('g-tag', gtag);
+customElements.define('hamburger-menu', hamburger);
--- en/components.js
+++ en/components.js
@@ -7,9 +7,54 @@ class SiteFooter extends HTMLElement {
         <br>
         Questions? Write to me at <a href="mailto:havard@iolden.no" style="color: rgb(203, 234, 255)">havard@iolden.no</a>
     </footer> `
-            
-        
+    }
+}
+
+class gtag extends HTMLElement {
+    connectedCallback() {
+        const gtagScript = document.createElement('script');
+        gtagScript.async = true;
+        gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-0LT1EF01DF';
+        document.head.appendChild(gtagScript);
+
+        const configScript = document.createElement('script');
+        configScript.innerHTML = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0LT1EF01DF');
+        `;
+        document.head.appendChild(configScript);
+    }
+}
+
+class hamburger extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `<div class="menu-content">
+        <a href="index.html" style="margin-top: 100px;">Home</a>
+        <a href="mainmap.html">Virtual guided trip</a>
+        <a href="picktrip.html">Hikes in the area</a>
+        <a href="sites.html">A short drive away</a>
+        <a href="weather.html">Weather forecast</a>
+        <a href="bus.html">Bus to Briksdalen</a>
+        <a href="toilet.html">Public toilets</a>
+        <a href="faq.html">Frequently asked questions</a>
+        <a href="comments.html">Digital guestbook</a>
+        <a href="feedback.html">Give feedback to the village</a>
+    </div>`; 
+
+        const hamMenu = document.querySelector('.ham-menu');
+        const menuContent = this.querySelector('.menu-content');
+
+        if (hamMenu && menuContent) {
+            hamMenu.addEventListener('click', () => {
+                hamMenu.classList.toggle('active');
+                menuContent.classList.toggle('active');
+            });
+        }
     }
 }
 
 customElements.define('site-footer', SiteFooter);
+customElements.define('g-tag', gtag);
+customElements.define('hamburger-menu', hamburger);
